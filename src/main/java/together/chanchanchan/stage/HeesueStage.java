16a5c7dc1d5fef04b5254bd5017a9fab0033d89c
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-16 05:31:32 +0900

    HeesueStage play메소드 매개변수 추가

--- src/main/java/together/chanchanchan/stage/HeesueStage.java
+++ src/main/java/together/chanchanchan/stage/HeesueStage.java
@@ -2,7 +2,7 @@ package together.chanchanchan.stage;
 
 public class HeesueStage extends GameController implements Game {
     @Override
-    public void play() {
+    public void play(String playName, String nowStyle) {
 
     }
 
